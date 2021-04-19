d01b17778904ce2be4dc16f20de1179676959981
Shahim Shaar <shahmshaar5@gmail.com>
2021-04-19 13:17:59 -0500

    modifications to routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,11 +10,6 @@ const routes = [
     component: Home
   },
   {
-    path: '/Welcome',
-    name: 'Welcome',
-    component: Welcome
-  },
-  {
     path: '/login',
     name: 'Login',
     component: () => import(/* webpackChunkName: "login" */ '@/views/Login')
