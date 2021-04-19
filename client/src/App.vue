03635650bf7175d86969c15b534cc46100267623
Charles Mennerick <charlesjm13@gmail.com>
2021-04-18 20:17:08 -0500

    remove persistent header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,12 @@
 <template>
     <div id="app">
     <NavBar />
-    <!-- think we still want the navbar here -->
+    <!-- think we still want the navbar here 
         <h3 style="text-align:center;color:gray;font-size:24px"> welcome to BECS, the #1 desitnation for college information</h3>
         <Welcome />
-
+    -->
         <main>
+        
         <router-view/>
         </main>
     </div>
@@ -13,11 +14,11 @@
 
 <script>
 import NavBar from '@/components/NavBar'
-import Welcome from '@/views/Welcome'
+//import Welcome from '@/views/Welcome'
 
 export default {
     name: 'app',
-    components: {Welcome, NavBar},
+    components: {/*Welcome,*/ NavBar},
 // removed NavBar from components
     created() {
         this.$store.dispatch('updateSubscribedSubvues')
