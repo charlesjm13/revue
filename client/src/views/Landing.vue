4132c6f86620252aa01852d5a047c47a3befea5c
Biishakha <bawale@knox.edu>
2021-04-26 14:20:28 -0500

    q/a changes

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -3,6 +3,8 @@
     <div class="txtcenter">
       <h1> Hello {{ $store.state.user.username }}</h1>
     </div>
+    <div class="container">
+      </div>
   </div>
 </template>
 
@@ -12,8 +14,24 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+*{
+  margin:0;
+padding:0;
+font-family: 'Roboto', sans-serif;
+}
+
 .txtcenter {
   text-align: center;
+}
+.container{
+  height: 100vh;
+  width: 100%;
+  background-image: url('../../public/assets/land2.jpg');
+  background-position: center;
+  background-size: cover;
+  
+
+
 }
 </style>
