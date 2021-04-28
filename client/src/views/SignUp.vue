eac60fac87e1b6b16e482435fdd1b2eef2081718
Emeka Nkuku <cgnkuku@knox.edu>
2021-04-28 13:07:04 -0500

    Added ability to choose selected tags; added proper path to profile page

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -1,36 +1,57 @@
 <template lang="html">
   <div class="signup">
-    <form @submit.prevent="signUp">
-      <h1>Sign Up</h1>
-      <p class="error" :class="{ 'deprecated-error' : deprecatedError }">{{ error }}</p>
-      <input type="text" v-model="firstname" placeholder="First Name">
-      <input type="text" v-model="lasttname" placeholder="Last Name">
-      <input type="text" v-model="username" placeholder="Username" ref="username">
-      <input type="email" v-model="email" placeholder="E-Mail">
-      <input type="password" v-model="password" placeholder="Password">
-      <input type="password" v-model="passwordRepeat" placeholder="Repeat Password">
-      <p v-if="!passwordsMatch" class="error">Passwords do not match!</p>
-      
-        <label for="user-types" style="float:left; padding-top: 5px; padding-left: 20px;">User Type:</label>
-      <div style="padding-bottom: 5px; padding-right: 20px;">
-          <select name="user-types" v-model="usertype" style="float:right; padding: 5px 0px 5px 0px;">
-           <option value="" disabled selected>Enter user type</option>
-           <option value="Current Student"> Current Student </option>
-           <option value="Prospective Student"> Prospective Student </option>
-          </select>
-      </div>
-      <br>
-      <div v-if="userType === 'Current Student'" style="padding-top: 20px">
-        <label for="current-school" style="float:left; padding-bottom: 25px; padding-top: 5px; padding-left: 20px;">Current School:</label>
-         <div style="padding-right: 20px;">
-          <select name="current-school" v-model="userschool" style="float:right; padding: 5px 28px 5px 0px;">
-           <option value="" disabled selected>Select a school</option>
-           <option value="Current Student"> Knox College </option>
-          </select>
+      <form @submit.prevent="signUp">
+          <h1>Sign Up</h1>
+          <p class="error" :class="{ 'deprecated-error' : deprecatedError }">{{ error }}</p>
+          <input type="text" v-model="firstname" placeholder="First Name">
+          <input type="text" v-model="lasttname" placeholder="Last Name">
+          <input type="text" v-model="username" placeholder="Username" ref="username">
+          <input type="email" v-model="email" placeholder="E-Mail">
+          <input type="password" v-model="password" placeholder="Password">
+          <input type="password" v-model="passwordRepeat" placeholder="Repeat Password">
+          <p v-if="!passwordsMatch" class="error">Passwords do not match!</p>
+
+          <label for="user-types" style="float:left; padding-top: 5px; padding-left: 20px;">User Type:</label>
+          <div style="padding-bottom: 5px; padding-right: 20px;">
+              <select name="user-types" v-model="usertype" style="float:right; padding: 5px 0px 5px 0px;">
+                  <option value="" disabled selected>Enter user type</option>
+                  <option value="Current Student"> Current Student </option>
+                  <option value="Prospective Student"> Prospective Student </option>
+              </select>
           </div>
-      </div>
-      <input class="button" type="submit" value="Sign Up">
-    </form>
+          <br>
+          <div v-if="userType === 'Current Student'" style="padding-top: 20px">
+              <label for="current-school" style="float:left; padding-bottom: 25px; padding-top: 5px; padding-left: 20px;">Current School:</label>
+              <div style="padding-right: 20px;">
+                  <select name="current-school" v-model="userschool" style="float:right; padding: 5px 28px 5px 0px;">
+                      <option value="" disabled selected>Select a school</option>
+                      <option value="Current Student"> Knox College </option>
+                  </select>
+              </div>
+          </div><br />
+          <label style="float:left; padding-bottom: 25px; padding-top: 5px; padding-left: 20px;">Tags:</label>
+          <div style="padding-bottom: 5px; padding-right: 20px;">
+              <select name="user-types" @change="addToTags($event)" v-model="usertags" style="float:right; padding: 5px 0px 5px 0px;">
+                  <option value="" disabled selected>Pick tags that relate to your interest</option>
+                  <!--<option v-for="tag in availableTags" :key="tag"></option>-->
+                  <option value="Math">Math</option>
+                  <option value="Science">Science</option>
+                  <option value="Literature">Literature</option>
+                  <option value="Art">Art</option>
+                  <option value="Business">Business</option>
+              </select><br /><br />
+          </div>
+
+          <div v-if="returnVal() > 0">
+          <label><strong><u>Your tags:</u></strong></label>
+          <div v-for="tag in chosenTags" :key="tag">
+              <button @click="removeFromTags(tag)" style="padding: 5px 5px 5px 0px;">Remove </button>
+              <label>&nbsp; {{tag}}</label>
+          </div>
+          </div>
+
+          <input class="button" type="submit" value="Sign Up">
+      </form>
   </div>
 </template>
 
@@ -51,7 +72,11 @@ export default {
       passwordRepeat: '',
       email: '',
       usertype: '',
-      userschool: ''
+      userschool: '',
+      usertags: '',
+      chosenTags: [],
+      val: 0,
+      availableTags: ["Math", "Science", "Reading", "Art", "Business"]
     }
   },
 
@@ -77,7 +102,21 @@ export default {
           this.error = e.response.data.error;
         })
       }
-    }
+      },
+
+      addToTags(event) {
+          this.chosenTags.push(event.target.value)
+          this.val = this.val + 1
+      },
+
+      removeFromTags(event){
+          this.chosenTags.splice(this.chosenTags.indexOf(event), 1)
+          this.val = this.val - 1
+      },
+
+      returnVal() {
+          return this.val
+      }
   },
 
   computed: {
@@ -135,7 +174,7 @@ export default {
   background-repeat: no-repeat;
 
   width: 100%;
-  height: calc(100vh - 49px);
+  height: calc(150vh - 0px);
   position: relative;
 
   display: flex;
