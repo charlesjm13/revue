98a3c39e9892ca495db661b2d414f53893da2323
Charles Mennerick <charlesjm13@gmail.com>
2021-04-28 13:23:45 -0500

    uniform styling to ratings components

--- client/src/views/CollegeRatings.vue
+++ client/src/views/CollegeRatings.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="home container" style="margin: auto; text-align: center;">
+  <div class="home-container" style="margin: auto; text-align: center; padding: 0px 200px 150px 200px;">
     <h1>College Ratings</h1>
     <div>
     <select v-model="selectedCollege">
@@ -30,5 +30,8 @@ export default {
 <style lang="css">
 select {
   background: #FAF7F2;
+}
+.home-container {
+  background: #779FA1;
 }
 </style>
