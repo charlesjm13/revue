5f2e7716454ba6bb60521b234a88a26582c3c83a
Charles Mennerick <charlesjm13@gmail.com>
2021-05-02 22:12:47 -0500

    fixed hover

--- client/src/views/QnA.vue
+++ client/src/views/QnA.vue
@@ -93,6 +93,53 @@ export default {
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+}
+
+ .img-text-wrapper {
+  position: absolute;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  text-align: center;
+  padding-left: 100px;
+  padding-right: 100px;
+}
+
+.img-text-wrapper .subtitle {
+  transition: 1s;
+  font-weight: 600;
+  color: transparent;
+}
+.img-text-wrapper:hover .subtitle {
+  font-weight: 600;
+  color: lightseagreen;
+}
+
+.img-darken {
+  transition: 1s;
+  filter: brightness(10%);
+}
+
+textarea {
+  width: 100%;
+  border: none;
+  font-family: tahoma;
+  font-size: 14px;
+  height: 60px;
+}
+
+#post-button {
+  float: right;
+  background-color: #88498f;
+  border: none;
+  color: white;
+  padding: 4px;
+  font-size: 14px;
+  border-radius: 2px;
+  width: 50px;
 
 }
 </style>
