ebfc97e8baf9c5d56899ac5168a66aa88f08bcd0
Shahim Shaar <shahmshaar5@gmail.com>
2021-05-30 06:33:22 -0500

    parsed json response to be able to display values nested in JSON object2

--- client/src/services/CollegeService.js
+++ client/src/services/CollegeService.js
@@ -2,8 +2,7 @@ import ScorecardApi from '@/services/ScorecardApi'
 //var params = new URLSearchParams("school.name","Knox College");
 export default {
   index() {
-    return ScorecardApi().get('?school.name=Knox%20College&_fields=id,school.name&api_key=erEtjmwQNDow4nEFMmMg4Y8EBslsmjqk4yP2E6bI', {params: {id: 146427}})
-  }
+    return ScorecardApi().get('?school.name=Knox%20College&_fields=id,school.name,school.state,school.school_url,school.religious_affiliation,school.accreditor&api_key=erEtjmwQNDow4nEFMmMg4Y8EBslsmjqk4yP2E6bI', {params: {id: 146427}})  }
 
   
 }
